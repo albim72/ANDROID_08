4c62ea112e656ae5b763ccbdad82df4b5983651f
albim72 <marcin.albiniak72@gmail.com>
2024-08-12 13:35:39 +0200

    Update MiaraAnglo.java

--- DZIEN_1/kalkulator_odl/MiaraAnglo.java
+++ DZIEN_1/kalkulator_odl/MiaraAnglo.java
@@ -2,10 +2,13 @@ package com.marcin.kalkulator_odl;
 
 public abstract class MiaraAnglo {
     protected double odl;
+    protected double mang;
 
-    public MiaraAnglo(double odl) {
+    public MiaraAnglo(double odl, double mang) {
         this.odl = odl;
+        this.mang = mang;
     }
+
     public abstract double policzOdl();
-    
+
 }
